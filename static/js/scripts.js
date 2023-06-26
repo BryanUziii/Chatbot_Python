6e1e959ee3615710b9ce6a26478927a4c7c6263b
BryanUziii <bryan.quintana2016@gmail.com>
2023-06-25 23:12:58 -0700

    Avance de las vistas

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,27 +1,81 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    const btnEnviar = document.getElementById("btnEnviar");
-    const inputEntrada = document.getElementById("entrada");
-  
-    const EnviarMensaje = () => {
-        let entrada = inputEntrada.value;
-      
-        // Crea un objeto FormData y agrega el valor de entrada al formulario
-        const formData = new FormData();
-        formData.append('entrada', entrada);
-      
-        // Realiza la solicitud POST con el formulario como datos
-        axios.post('/chatbot', formData)
-          .then(function(response) {
-            const respuesta = response.data;
-            console.log(respuesta);
-          })
-          .catch(function(error) {
-            console.error(error);
-          });
+     const btnLimpiar = document.getElementById("btnLimpiar");
+
+
+      function limpiarChat(){
+        var chatDiv = document.getElementById('chat');
+        chatDiv.innerHTML = "";
       }
+
+      // Función para agregar un mensaje al chat
+      function addMessage(message, sender) {
+        var chatDiv = document.getElementById('chat');
+        var messageDiv = document.createElement('div');
+        messageDiv.className = 'message ' + (sender === 'Tú' ? 'user-message' : 'bot-message');
+        var contentDiv = document.createElement('div');
+        contentDiv.className = 'message-content';
+
+
+
+
+        // contentDiv.innerText = sender + ': ' + message;
+
+        if (sender === 'Tú') {
+          // Acciones si el remitente es "Tú" (usuario)
+          console.log('Mensaje enviado por el usuario');
+          contentDiv.innerText =  message;
+        } else {
+          // Acciones si el remitente no es "Tú" (chatbot)
+          console.log('Mensaje enviado por el chatbot');
+
+          // Crea un objeto FormData y agrega el valor de entrada al formulario
+          const formData = new FormData();
+          formData.append('entrada', message);
+
+
+          // Realiza la solicitud POST con el formulario como datos
+          axios.post('/chatbot', formData)
+            .then(function(response) {
+              const respuesta = response.data;
+              console.log(respuesta);
+              contentDiv.innerText = respuesta;
+            })
+            .catch(function(error) {
+              console.error(error);
+            });
+        }
+
+
+        messageDiv.appendChild(contentDiv);
+        chatDiv.appendChild(messageDiv);
+
+        // Scroll automático hacia el último mensaje
+        chatDiv.scrollTop = chatDiv.scrollHeight;
+
+
+      }
+
+      // Evento de envío de mensaje
+      document.getElementById('chat-inputs').addEventListener('submit', function(event) {
+        event.preventDefault();
+        var messageInput = document.getElementById('inputMensaje');
+        var message = messageInput.value;
+
+        // Agregar el mensaje enviado al chat
+        addMessage(message, 'Tú');
+        addMessage(message, 'Chatbot');
+
+        // Simular respuesta después de 1 segundo
+        // setTimeout(function() {
+        //   addMessage(message, 'Chatbot');
+        // }, 1000);
+
+        // Limpiar el campo de entrada
+        messageInput.value = '';
+      });
   
-    btnEnviar.addEventListener("click", EnviarMensaje);
+      btnLimpiar.addEventListener("click", limpiarChat);
 
   });
   
